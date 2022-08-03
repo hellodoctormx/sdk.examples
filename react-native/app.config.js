bdbccf9d869928718720f45c0b58621c45aec328
Daniel Sarfati <daniel@hellodoctor.com.mx>
2022-08-03 08:36:00 -0500

    fixup! fixup! wrapped up a number of UI kit components - still need to organize components though

--- react-native/app.config.js
+++ react-native/app.config.js
@@ -1,3 +1,3 @@
+export const API_KEY = '';
 export const API_HOST = 'https://third-party-demo-service-pusuheofiq-uc.a.run.app';
-export const API_KEY = '8C7594BDFF94B47A14C63BE7A1918';
 export const HELLO_DOCTOR_API_HOST = 'https://public-api-pusuheofiq-uc.a.run.app';
